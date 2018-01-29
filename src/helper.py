de34c93e4e35e3acda5274e32f1198264f364044
Chris Blair <chris.blair36@gmail.com>
2018-01-28 20:11:52 -0800

    Add Helper module and tests

---
+++ src/helper.py
@@ -0,0 +1,20 @@
+""" Helper functions shared between 2 or more modules"""
+
+def checkIfInt(value):
+    """ Type checker for values to determine if they are of type int """
+    if not isinstance(value, int):
+        raise TypeError(("value is not an int. "
+                         "value = {0}").format(value))
+
+def checkIfCoordinateIsValid(x, y):
+    """ Check if board values constitute a valid location on the board """
+    valid = False
+    even_values = (1, 3, 5, 7, 9)
+    odd_values = (2, 4, 6, 8, 0)
+    if y in odd_values:
+        if x in odd_values:
+            valid = True
+    elif y in even_values:
+        if x in even_values:
+            valid = True
+    return valid
