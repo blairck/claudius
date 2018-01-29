0d9a581d6773fc7553c3870a16b50c444b387377
Chris Blair <chris.blair36@gmail.com>
2018-01-28 20:12:59 -0800

    Fix a Gamenode test and remove others

--- test/test_gamenode.py
+++ test/test_gamenode.py
@@ -12,7 +12,7 @@ class TestGameNode(unittest.TestCase):
         """ Test a known default instantiation """
         gn_obj = gamenode.GameNode()
         result = gn_obj.gameState[0][0]
-        self.assertEqual(result, -1)
+        self.assertEqual(result, 0)
         self.assertFalse(gn_obj.leafP)
         self.assertFalse(gn_obj.rootP)
         self.assertFalse(gn_obj.score)
@@ -33,26 +33,4 @@ class TestGameNode(unittest.TestCase):
                               "0  0  0  0  0  0  0  0  0  0\n"
                               "0  0  0  0  0  0  0  0  0  0\n"
                               "0  0  0  0  0  0  0  0  0  0")
-            self.assertEqual(actual_print, expected_print)
-
-    def test_print_middle_rows_without_seperator(self):
-        """ Check that print_middle_rows() works without seperator """
-        with helper.captured_output() as out:
-            gn_obj = gamenode.GameNode()
-            gn_obj.print_middle_rows()
-            actual_print = out.getvalue().strip()
-            expected_print = ("0000000\n"
-                              "0000000\n"
-                              "0000000")
-            self.assertEqual(actual_print, expected_print)
-
-    def test_print_middle_rows_with_seperator(self):
-        """ Check that print_middle_rows() works with a seperator """
-        with helper.captured_output() as out:
-            gn_obj = gamenode.GameNode()
-            gn_obj.print_middle_rows(1)
-            actual_print = out.getvalue().strip()
-            expected_print = ("0101010101010\n"
-                              "0101010101010\n"
-                              "0101010101010")
             self.assertEqual(actual_print, expected_print)
