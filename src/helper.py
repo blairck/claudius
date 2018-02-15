8b78dae0b5fb7c354980878244b34fe792b1f42f
Chris Blair <chris.blair36@gmail.com>
2018-02-14 23:10:34 -0800

    Add basic main.py file and fix bug with coordinate validity check

--- src/helper.py
+++ src/helper.py
@@ -9,13 +9,13 @@ def checkIfInt(value):
 def checkIfCoordinateIsValid(x, y):
     """ Check if board values constitute a valid location on the board """
     valid = False
-    even_values = (1, 3, 5, 7, 9)
-    odd_values = (2, 4, 6, 8, 0)
-    if y in odd_values:
-        if x in odd_values:
-            valid = True
-    elif y in even_values:
+    odd_values = (1, 3, 5, 7, 9)
+    even_values = (2, 4, 6, 8, 10)
+    if y in even_values:
         if x in even_values:
+            valid = True
+    elif y in odd_values:
+        if x in odd_values:
             valid = True
     if valid:
         return True
