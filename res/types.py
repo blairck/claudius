0f46d61dfb97a7b18e36f0167047a992fa809e8a
Chris Blair <chris.blair36@gmail.com>
2018-09-28 21:20:30 -0700

    Update strange style of conditional

--- res/types.py
+++ res/types.py
@@ -9,17 +9,17 @@ def getPieceAbbreviation(value):
     if int(value)==OFF_BOARD:
         return '   '
     elif int(value)==EMPTY:
-        return '|||'
-    if int(value)==PLAYER_A_REGULAR:
+        return '| |'
+    elif int(value)==PLAYER_A_REGULAR:
         return '|a|'
-    if int(value)==PLAYER_A_KING:
+    elif int(value)==PLAYER_A_KING:
         return '|A|'
-    if int(value)==PLAYER_B_REGULAR:
+    elif int(value)==PLAYER_B_REGULAR:
         return '|b|'
-    if int(value)==PLAYER_B_KING:
+    elif int(value)==PLAYER_B_KING:
         return '|B|'
     else:
-        raise TypeError(("Invalid piece value = {0}").format(value))
+        raise TypeError(("Invalid piece int = {0}").format(value))
 
 def getPieceIntValueFromChar(value):
     if value==' ':
