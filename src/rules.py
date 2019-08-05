9dfff7361d5916a8bd88bcf2b2c1db528a66c7d9
Chris Blair <chris.blair36@gmail.com>
2019-08-04 17:32:11 -0700

    Remove unused code and tests

--- src/rules.py
+++ src/rules.py
@@ -4,40 +4,6 @@
 from res import types
 from src import coordinate
 
-def isLegalMove(theGame, startCoordinate, endCoordinate):
-    """"Tests whether a start coordinate and end coordinate constitute a
-    legal move"""
-    """Ideally, this just takes a board state, start/end coordinates
-    and returns true or false"""
-    if (theGame.getState(startCoordinate) == types.PLAYER_A_REGULAR and
-            endCoordinate.get_y_board() < startCoordinate.get_y_board()):
-        return False
-    elif (theGame.getState(startCoordinate) == types.PLAYER_B_REGULAR and
-          endCoordinate.get_y_board() > startCoordinate.get_y_board()):
-        return False
-    elif (theGame.getState(endCoordinate) == types.EMPTY and
-          findConnectionP(startCoordinate, endCoordinate)):
-        return True
-    return False
-
-def findConnectionP(startCoordinate, endCoordinate):
-    """Finds connection between start coordinate and end coordinate.
-    Returns True if connection exists, False otherwise"""
-    startX = startCoordinate.get_x_board()
-    startY = startCoordinate.get_y_board()
-    endX = endCoordinate.get_x_board()
-    endY = endCoordinate.get_y_board()
-
-    if endX - startX == 1 and endY - startY == 1:
-        return True
-    elif endX - startX == 1 and endY - startY == -1:
-        return True
-    elif endX - startX == -1 and endY - startY == -1:
-        return True
-    elif endX - startX == -1 and endY - startY == 1:
-        return True
-    return False
-
 def findXDeltaFromDirection(direction):
     """ Returns delta X for jumping, when given a direction value """
     if direction in (2, 4):
